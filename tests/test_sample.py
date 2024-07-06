ef3d284dd35259ab2b6901c6bbdaf6eb2f07cba6
Yswd <42904813+yuk12ama6a@users.noreply.github.com>
2024-07-06 23:44:41 +0900

    Delete #12 サンプルコードの削除

--- tests/test_sample.py
+++
@@ -1,9 +0,0 @@
-from src.sample import Sample
-
-
-class TestSample:
-
-    def test_say_hello(self) -> None:
-        actual = Sample(name="Yswd").say_hello()
-
-        assert actual == "Hello, Yswd"
