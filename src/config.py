b140e300cf639697cf757b113ee02d30f8339d2c
Yswd <42904813+yuk12ama6a@users.noreply.github.com>
2024-07-15 00:00:30 +0900

    Add #18 Postgres用の環境変数の追加

--- src/config.py
+++ src/config.py
@@ -7,8 +7,24 @@ class Settings(BaseSettings):
 
     Attributes
         - VERSION (str): バージョン
+        - POSTGRES_USERNAME (str): PostgreSQLのユーザ名
+        - POSTGRES_PASSWORD (str): PostgreSQLのパスワード
+        - POSTGRES_HOST (str): PostgreSQLのホスト名
+        - POSTGRES_PORT (int): PostgreSQLのポート番号
+        - POSTGRES_DB_NAME (str): PostgreSQLのデータベース名
     """
 
+    # Core
     VERSION: str = "0.0.0"
 
+    # DB
+    POSTGRES_USERNAME: str = ""
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB_NAME: str = "postgres"
+
     model_config = SettingsConfigDict(env_file="./env/.env", env_file_encoding="utf-8")
+
+    def get_database_url(self) -> str:
+        return f"postgresql://{self.POSTGRES_USERNAME}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB_NAME}"
